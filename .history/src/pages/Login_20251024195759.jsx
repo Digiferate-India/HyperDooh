dbc5e9ffd47fcb3e22afa89c352ed897643d6818
Tanishq Pratap <tanishqontop@gmail.com>
2025-10-24 20:00:10 +0530

    login field fixed

--- .history/src/pages/Login_20251024195759.jsx
+++ .history/src/pages/Login_20251024195759.jsx
@@ -81,7 +81,7 @@ const Login = () => {
                 placeholder="name@gmail.com"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
-                className="w-full mt-1 px-4 py-3 border bg-white text-gray-900 border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                className="w-full mt-1 px-4 py-3 border text-gray-900 border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                 required
               />
             </div>
@@ -94,7 +94,7 @@ const Login = () => {
                 placeholder="6+ characters"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
-                className="w-full mt-1 px-4 py-3 border bg-white text-gray-900 border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                className="w-full mt-1 px-4 py-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                 required
               />
               <button
